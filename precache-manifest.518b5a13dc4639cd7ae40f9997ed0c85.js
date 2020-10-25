2a727d86ba843cdd7f7fa1e3491e1eb589372bdc
Olego0 <ostrovskij.013g@gmail.com>
2020-10-25 10:23:02 +0200

    Updates

---
+++ precache-manifest.518b5a13dc4639cd7ae40f9997ed0c85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a71efb7c7b1b5e1e9c1b001b970c91f",
+    "url": "/ReactJS/index.html"
+  },
+  {
+    "revision": "256e80c1eceb34ee6fea",
+    "url": "/ReactJS/static/css/main.ee71ac88.chunk.css"
+  },
+  {
+    "revision": "ebcffc63d24bf9b22d97",
+    "url": "/ReactJS/static/js/2.3c6ee2a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS/static/js/2.3c6ee2a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "256e80c1eceb34ee6fea",
+    "url": "/ReactJS/static/js/main.c6cfc6ff.chunk.js"
+  },
+  {
+    "revision": "b89e94b6d774650b4327",
+    "url": "/ReactJS/static/js/runtime-main.3cc60b21.js"
+  }
+]);
